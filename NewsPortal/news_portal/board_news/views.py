e64e744832db39e67e6aa0198f032ea98e0f9082
Anton <an.vaseko@mail.ru>
2025-02-10 22:54:48 +0300

    Из views.py убрать переопределение get_context_data

--- NewsPortal/news_portal/board_news/views.py
+++ NewsPortal/news_portal/board_news/views.py
@@ -10,11 +10,6 @@ class NewList(ListView):
     template_name = 'flatpages/news.html'
     context_object_name = 'news'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        return context
-
 
 class NewDetail(DetailView):
     model = Post
